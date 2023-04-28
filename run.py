9f8418d2789b99de1a79cec890bfca26e7e7120c
Дмитрий <sudakov.di@edu.spbstu.ru>
2023-04-28 03:00:22 +0300

    modified:   index.html 	new file:   log/log-20230428-023746988385.txt 	new file:   log/log-20230428-023928879004.txt 	new file:   log/log-20230428-024025882003.txt 	new file:   log/log-20230428-024031120972.txt 	new file:   log/log-20230428-024258240707.txt 	new file:   log/log-20230428-024445095670.txt 	new file:   log/log-20230428-024457384528.txt 	new file:   log/log-20230428-024613148034.txt 	new file:   log/log-20230428-024954680997.txt 	new file:   log/log-20230428-025328988525.txt 	new file:   log/log-20230428-025725822414.txt 	new file:   log/log-20230428-025728456565.txt 	new file:   log/log-20230428-025828860564.txt 	new file:   log/log-20230428-025913661902.txt 	new file:   log/log-20230428-025929220627.txt 	modified:   run.py 	new file:   templates/faculties_template.html 	modified:   templates/teachers_template.html

--- run.py
+++ run.py
@@ -7,12 +7,23 @@ async def get_teachers_routine():
         res = await api.get_teachers()
     return res
 
+async def get_faculties_routine():
+    res = "teachers"
+    async with ApiSpbStuRuz.ApiSpbStuRuz() as api:  
+        res = await api.get_faculties()
+    return res
+
 app = Flask(__name__)
 
 @app.route("/teachers", methods=["GET"])
 async def teachers():
     result = await get_teachers_routine()
     return render_template('teachers_template.html', teachers=result)
+
+@app.route("/faculties", methods=["GET"])
+async def faculties():
+    result = await get_faculties_routine()
+    return render_template('faculties_template.html', faculties=result)
 
 @app.route("/", methods=["GET"])
 def main_page():
