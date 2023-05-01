888e69d1ce4d61f04ba47f670db4550f81c310a5
ShizzzaL <qwr@example.com>
2023-05-02 00:57:18 +0300

    Changes to be committed: 	modified:   database/login_user.py

--- database/login_user.py
+++ database/login_user.py
@@ -16,6 +16,18 @@ def try_select_by_username(login):
     return row[0] 
 
 
+def get_user_email(login):
+    conn = get_connection(postgres_ctx)
+    cur = conn.cursor()
+    cur.execute(f"SELECT email FROM users WHERE username = '{login}';")
+    row = cur.fetchone()
+    cur.close()
+    conn.close()
+    if not row:
+        return None
+    return row[0] 
+
+
 def login_user_proxy(login, password):
     # Check if the login and password are valid (for example, by looking them up in a database)
     id = try_select_by_username(login)
@@ -30,6 +42,15 @@ def login_user_proxy(login, password):
             login_user(user)
         except Exception:
             return "Unable to login"
-        return redirect(url_for('main'))
+        try:
+            email = get_user_email(user)
+            #otp_secret = otp.generate_secret()
+            #save_user_otp_email(username, email)
+            #msg = Message('One-Time Password for Sign In', sender=app.config['MAIL_USERNAME'], recipients=[email])
+            #msg.body = f'Your one-time password for sign in: {otp.generate_otp(otp_secret)}'
+            #send_email(message, reciever)
+        except Exception:
+            return "Two-factor authentication fail"
+        return redirect(url_for('verify', username=username))
     else:
         return "user does not exist"
